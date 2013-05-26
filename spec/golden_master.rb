fa42d8948d111ce89a2e10bb18324dfaa822cafd
Rafe Colton <r.colton@modcloth.com>
2013-05-26 15:52:46 -0400

    Reorganizing some tests

---
+++ spec/golden_master.rb
@@ -0,0 +1,34 @@
+require 'spec/spec_helper'
+
+describe 'using a profile and then restoring' do
+  it 'leaves the files unchanged after restoring' do
+    files = file_list(DEFAULT_PROFILE_DIR)
+    hash_list_before = home_hash_list(files)
+
+    # TODO: use the default profile
+    # TODO: restore from backup
+
+    hash_list_after = home_hash_list(files)
+
+    hash_list_before.must_equal hash_list_after
+  end
+end
+
+def file_list(dir)
+  `find #{dir} -xdev -type f`.
+    chomp.
+    split("\n").
+    map{ |f| f.gsub(/^#{dir}\//, '') }
+end
+
+def home_hash_list(file_list = [])
+  file_list.map do |file|
+    `#{<<-EOB}`.chomp
+    if [ -f "$HOME/#{file}" ] ; then
+      md5 -q "$HOME/#{file}"
+    else
+      md5 -qs 'not present'
+    fi
+  EOB
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'minitest/spec'
+
+GOLD = "\033\[33;1m"
+RESET = "\033\[0m"
+GREEN = "\033\[32m"
+RED = "\033\[31m"
+BRIGHT_GREEN = "\033\[32;1m"
+BRIGHT_RED = "\033\[31;1m"
+DEFAULT_PROFILE_DIR = File.expand_path('./profiles/default')
