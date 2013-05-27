e108558f8059450a2656452674d7dbc592d234e3
Rafe Colton <r.colton@modcloth.com>
2013-05-27 07:48:04 -0700

    Using `shell_out` helper function for specs too

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,11 @@ RED = "\033\[31m"
 BRIGHT_GREEN = "\033\[32;1m"
 BRIGHT_RED = "\033\[31;1m"
 DEFAULT_PROFILE_DIR = File.expand_path('./profiles/default')
+
+def shell_out(command = '', show_output = false)
+  system "#{command}#{show_output ? '' : ' 1>/dev/null'}"
+  unless $? == 0
+    $stderr.puts %Q(#{RED}Error running `#{command}`#{RESET})
+    exit $?.to_i
+  end
+end
